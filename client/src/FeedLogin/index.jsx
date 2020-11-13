0bd0718380fa9af6af942e1f37923d90310ce6d6
Evan Coulson <evan.m.coulson@gmail.com>
2020-11-13 00:40:01 -0700

    drop down is togglable

--- client/src/FeedLogin/index.jsx
+++ client/src/FeedLogin/index.jsx
@@ -1,21 +1,28 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as UserIcon } from "../Assets/user.svg";
 import "./index.css";
 
 export default function FeedLogin() {
+    const [showing, toggleDropdown] = useState(false);
+
+    function handleClick() {
+        toggleDropdown(!showing);
+    }
+
     return (
         <div className="feed-login">
             <div className="profile-image">
-                <UserIcon className="profile-image-avatar-placeholder" />
+                <UserIcon onClick={handleClick} className="profile-image-avatar-placeholder" />
             </div>
-            <div className="profile-dropdown">
+            {showing ? <div className="profile-dropdown">
                 <Link className="profile-dropdown-link" to="/me">Me</Link>
                 <Link className="profile-dropdown-link" to="/setup-account">Link Accounts</Link>
                 <Link className="profile-dropdown-link" to="/edit-profile">Settings</Link>
                 <div className="horizontal-rule"></div>
                 <Link className="profile-dropdown-link" to="/logout">Logout</Link>
-            </div>
+            </div> : null}
         </div>
     )
 }
