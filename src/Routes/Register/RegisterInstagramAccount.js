6a38077b470b52e6baf3e37312b843c7bf934d48
Evan Coulson <evan.m.coulson@gmail.com>
2020-12-05 15:29:09 -0700

    brought up to date

--- src/Routes/Register/RegisterInstagramAccount.js
+++ src/Routes/Register/RegisterInstagramAccount.js
@@ -19,7 +19,7 @@ const TOKEN_PATH = 'token.json';
 fs.readFile('credentials.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
     // Authorize a client with credentials, then call the Gmail API.
-    //   authorize(JSON.parse(content), setupInstagram);
+    authorize(JSON.parse(content), setupInstagram);
 });
 
 /**
